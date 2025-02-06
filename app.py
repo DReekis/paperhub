63bb3db93b8bc124e680931061d34c7e9c98ef21
Dreekis <rickghosh86@gmail.com>
2025-02-06 22:49:17 +0530

    6th_feb

--- app.py
+++ app.py
@@ -160,4 +160,4 @@ def download_file(file_url):
     return send_file(io.BytesIO(response.content), download_name=file_url.split('/')[-1])
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
